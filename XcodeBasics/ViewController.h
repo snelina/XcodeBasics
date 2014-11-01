09dab1e9805e3a9ba1bff95936be5d0ec410e669
E <selinak@Selinaui-MacBook-Pro.local>
2014-11-01 10:47:57 -0700

    Initial Commit

---
+++ XcodeBasics/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XcodeBasics
+//
+//  Created by E on 11/1/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
